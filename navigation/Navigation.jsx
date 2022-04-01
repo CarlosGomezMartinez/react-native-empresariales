fb626680ea7d3ba70bcacba4b9d65c7c51ec490b
Esteban Restrepo <esteban.restrepo@amadeus.com>
2022-04-01 15:21:16 -0500

    update layur and navigation for the app

--- navigation/Navigation.jsx
+++ navigation/Navigation.jsx
@@ -1,27 +1,26 @@
-import { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
+
 import Home from '../pages/Home';
 import Results from '../pages/Results';
 
 const Stack = createNativeStackNavigator();
 
-const Navigation = ({onSearch}) => {
-  const navigation = useNavigation()
-  useEffect(() => {
-    if(onSearch) {
-      navigation.navigate('Results', onSearch )
-    }
-  }, [onSearch]);
+const Navigation = () => {
 
   return (
-    <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-      <Stack.Screen name="Results" component={Results} />
-   </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+        <Stack.Screen 
+          name="Results"
+          component={Results}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
