c59ecb560e5554deb9d43cc34143a7f59b853d85
Doly Jimenez <doly.jimenez11@gmail.com>
2022-04-07 08:07:36 -0500

    add NumberFormat dependencie for price

--- components/Cardview.jsx
+++ components/Cardview.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, ScrollView} from "react-native";
+import { View, ScrollView, StyleSheet} from "react-native";
 import { Text, Card, Button } from "react-native-elements";
+import NumberFormat from "react-number-format";
 
 const Cardview = ({item, showInfo = true, navigation}) => {
   return (
     <ScrollView>
-    <View style={{alignItems: 'center', justifyContent:'center', flex:1}}>      
+    <View style={styles.containerCard}>      
       <Card containerStyle={{borderRadius:8, borderWidth: 1, borderColor: '#E2E2E2', borderStyle:'solid', width: '65%', height:335, opacity:1, elevation:5}}>         
         <Card.Image       
           style = {{resizeMode: 'contain', width:'100%', padding:0}}
@@ -19,8 +20,10 @@ const Cardview = ({item, showInfo = true, navigation}) => {
         <>
           <Text style={{margin: 0, textAlign: "center", fontFamily:'poppins-medium', fontSize: 13}}>{item.name}</Text>
           <Text style={{color: '#772CE8', textAlign: "center", fontFamily:'poppins-semibold', fontSize: 11,}}>{item.brand}</Text>
-          <Text style={{textAlign: "center", fontFamily:'poppins-medium', fontSize: 12}}>{item.city}</Text>          
-          <Text style={{textAlign: "center", marginBottom: 5, fontFamily:'poppins-medium', fontSize: 12}}>{item.price}</Text>
+          <Text style={{textAlign: "center", fontFamily:'poppins-medium', fontSize: 12}}>{item.city}</Text> 
+          <View style={styles.price}>       
+          <NumberFormat value={item.price} displayType={'text'} thousandSeparator={true} prefix={'$'}></NumberFormat>
+          </View>
           <Text style={{textAlign: "center", fontFamily:'poppins-medium', fontSize: 12}}>{item.rating}</Text>
 
           <Button    
@@ -37,5 +40,17 @@ const Cardview = ({item, showInfo = true, navigation}) => {
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  containerCard:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent:'center'    
+  },
+
+  price: {
+    fontFamily:'poppins-medium', fontSize: 12, textAlign: "center"
+  }  
+});
 
 export default Cardview;
